5e85a22a25e6c55cd049d5990545cb1593639688
Rikas Mohomed <rmohomed@zapmii.com>
2022-11-02 00:55:37 +0530

    Gangman drawing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,34 @@
-import { useState } from "react"
+import { useState } from "react";
+import { HangmanDrawing } from "./HangmanDrawing";
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./Keyboard";
+import words from "./wordList.json";
 
 function App() {
-  
-  const [wordToGuess,setWordToGuess] = useState("Test")
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  const [guessedLetters, setguessedLetters] = useState<string[]>([]);
+
+  console.log(wordToGuess);
   return (
-    <h1> Rikas</h1>
-  )
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        margin: "0 auto",
+        alignItems: "center",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center" }}>Lose Win</div>
+      <HangmanDrawing />
+      <HangmanWord />
+      <Keyboard />
+    </div>
+  );
 }
 
-export default App
+export default App;
