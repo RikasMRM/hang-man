54eaee2d0edc5142a6247e11fbec908f4408ff92
Rikas Mohomed <rmohomed@zapmii.com>
2022-11-03 22:26:59 +0530

    Gangman keyboard

---
+++ src/Keyboard.tsx
@@ -0,0 +1,54 @@
+import styles from "./Keyboard.module.css"
+
+const KEYS = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+export function Keyboard() {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(70px, 1fr))",
+        gap: ".5rem",
+      }}
+    >
+      {KEYS.map((key) => {
+        return (
+        <button 
+            // disabled
+            className={`${styles.btn} `} 
+            key={key}
+            >
+                {key}
+            </button>
+        )
+      })}
+    </div>
+  );
+}
